949b3cae36a4b199c8b1f9606ed97b01a186fda8
courbije <jeremie.courbis@gmail.com>
2015-03-14 21:44:09 +0100

    add : -Enumeration -class : 	-abstract 	-attributs 	-operations missing : - attributs Visbility and cardinality

--- macros/GenOCL.py
+++ macros/GenOCL.py
@@ -1,31 +1,31 @@
 """
-=========================================================
-                       GenOCL.py
- Generate a USE OCL specification from a UML package
-=========================================================
-
-FILL THIS SECTION AS SHOWN BELOW AND LINES STARTING WITH ###
-@author Xuan Shong TI WONG SHI <xuan.ti@mydomain.com>
-@author Maria Shohie CEZAR LOPEZ DE ANDERA <maria.cezar@ujf-grenoble.fr>
-@group  G99
-
-Current state of the generator
-----------------------------------
-FILL THIS SECTION 
-Explain which UML constructs are supported, which ones are not.
-What is good in your generator?
-What are the current limitations?
-
-Current state of the tests
---------------------------
-FILL THIS SECTION 
-Explain how did you test this generator.
-Which test are working? 
-Which are not?
-
-Observations
-------------
-Additional observations could go there
+    =========================================================
+    GenOCL.py
+    Generate a USE OCL specification from a UML package
+    =========================================================
+    
+    FILL THIS SECTION AS SHOWN BELOW AND LINES STARTING WITH ###
+    @author Xuan Shong TI WONG SHI <xuan.ti@mydomain.com>
+    @author Maria Shohie CEZAR LOPEZ DE ANDERA <maria.cezar@ujf-grenoble.fr>
+    @group  G99
+    
+    Current state of the generator
+    ----------------------------------
+    FILL THIS SECTION
+    Explain which UML constructs are supported, which ones are not.
+    What is good in your generator?
+    What are the current limitations?
+    
+    Current state of the tests
+    --------------------------
+    FILL THIS SECTION
+    Explain how did you test this generator.
+    Which test are working?
+    Which are not?
+    
+    Observations
+    ------------
+    Additional observations could go there
 """
 
 
@@ -33,50 +33,49 @@ Additional observations could go there
 #   Helpers on the source metamodel (UML metamodel)
 #---------------------------------------------------------
 # The functions below can be seen as extensions of the
-# modelio metamodel. They define useful elements that 
+# modelio metamodel. They define useful elements that
 # are missing in the current metamodel but that allow to
 # explorer the UML metamodel with ease.
-# These functions are independent from the particular 
-# problem at hand and could be reused in other 
+# These functions are independent from the particular
+# problem at hand and could be reused in other
 # transformations taken UML models as input.
 #---------------------------------------------------------
 
 # example
 def isAssociationClass(element):
-    """ 
-    Return True if and only if the element is an association 
-    that have an associated class, or if this is a class that
-    has a associated association. (see the Modelio metamodel
-    for details)
     """
-    # TODO
-    
- 
+        Return True if and only if the element is an association
+        that have an associated class, or if this is a class that
+        has a associated association. (see the Modelio metamodel
+        for details)
+    """
+# TODO
+
+
 #---------------------------------------------------------
 #   Application dependent helpers on the source metamodel
 #---------------------------------------------------------
 # The functions below are defined on the UML metamodel
 # but they are defined in the context of the transformation
 # from UML Class diagramm to USE OCL. There are not
-# intended to be reusable. 
-#--------------------------------------------------------- 
+# intended to be reusable.
+#---------------------------------------------------------
 
 # example
 def associationsInPackage(package):
     """
-    Return the list of all associations that start or
-    arrive to a class which is recursively contained in
-    a package.
+        Return the list of all associations that start or
+        arrive to a class which is recursively contained in
+        a package.
     """
-    
 
-    
+
 #---------------------------------------------------------
 #   Helpers for the target representation (text)
 #---------------------------------------------------------
 # The functions below aims to simplify the production of
-# textual languages. They are independent from the 
-# problem at hand and could be reused in other 
+# textual languages. They are independent from the
+# problem at hand and could be reused in other
 # transformation generating text as output.
 #---------------------------------------------------------
 
@@ -102,71 +101,105 @@ def associationsInPackage(package):
 
 def umlEnumeration2OCL(enumeration):
     """
-    Generate USE OCL code for the enumeration
-    """
-    print "%s" % enumeration
+        Generate USE OCL code for the enumeration
+        """
+    x=""
+    print "enum %s {" % enumeration.getName()
+    for e in enumeration.getValue():
+        x+= "\t%s,\n" % e.getName()
+    x=x[:-2]
+    print x
+    print "}"
 
 def umlBasicType2OCL(basicType):
     """
-    Generate USE OCL basic type. Note that
-    type conversions are required.
+        Generate USE OCL basic type. Note that
+        type conversions are required.
     """
     t = basicType.getName()
     if (t == "integer") :
         return "Integer"
     elif (t == "string") :
-        return "String"    
+        return "String"
     elif (t == "boolean") :
         return "Boolean"
     elif (t == "float") :
         return "Real"
     else:
         return t
+
+
+
+def umlOperations2OCL(operations):
+    if (operations):
+        print "operation"
+    for operation in operations:
+        name = operation.getName()
         
-    
-def umlAttribute2OCL(attribute):
-    a = attribute.getName()
-    b = umlBasicType2OCL(attribute.getType())
-    print "\t" + a + " : " + b
-    
+        #recuperation des parametres
+        parameters = ""
+        for parameter in operation.getIO():
+            parameters += "%s : %s," % (parameter.getName(),umlBasicType2OCL(parameter.getType()))
+        if (operation.getIO()):
+            parameters=parameters[:-1]
+
+        #valeur de retour
+        type = " : %s" % umlBasicType2OCL(operation.getReturn().getType()) if operation.getReturn() else ""
+        print "\t%s(%s)%s" % (name,parameters,type)
+
+
+def umlAttributes2OCL(attributes):
+    if (attributes):
+        print "attributes"
+    for attribute in attributes:
+        name = attribute.getName()
+        type = umlBasicType2OCL(attribute.getType())
+        annotation = "-- @derived" if attribute.isDerived else ""
+        print "\t%s : %s%s" % (name ,type,annotation)
+
 def umlClass2OCL(clas):
     """
-    Generate USE OCL code for the class
+        Generate USE OCL code for the class
     """
-    print "class %s" % clas.getName()
-    print "attributes"    
-    for attribute in clas.getOwnedAttribute():
-        umlAttribute2OCL(attribute)
+
+    abstract = " abstract" if clas.isAbstract else ""
+    print "%sclass %s" % (abstract,clas.getName())
+    umlAttributes2OCL(clas.getOwnedAttribute())
+    umlOperations2OCL(clas.getOwnedOperation())
+    print "end\n"
+
 
 def package2OCL(package):
     """
-    Generate a complete OCL specification for a given package.
-    The inner package structure is ignored. That is, all
-    elements useful for USE OCL (enumerations, classes, 
-    associationClasses, associations and invariants) are looked
-    recursively in the given package and output in the OCL
-    specification. The possibly nested package structure that
-    might exist is not reflected in the USE OCL specification
-    as USE is not supporting the concept of package.
+        Generate a complete OCL specification for a given package.
+        The inner package structure is ignored. That is, all
+        elements useful for USE OCL (enumerations, classes,
+        associationClasses, associations and invariants) are looked
+        recursively in the given package and output in the OCL
+        specification. The possibly nested package structure that
+        might exist is not reflected in the USE OCL specification
+        as USE is not supporting the concept of package.
     """
     for c in package.getOwnedElement():
-      if isinstance(c, Enumeration) :
-         umlEnumeration2OCL(c)
-      if isinstance(c, Class) :
-         umlClass2OCL(c)
-    
-for c in selectedElements:
-   for package in c.getOwnedElement():
-      package2OCL(package)
+        if isinstance(c, Enumeration) :
+            umlEnumeration2OCL(c)
+        if isinstance(c, Class):
+            umlClass2OCL(c)
+
+#for c in selectedElements:
+#   for package in c.getOwnedElement():
+#      package2OCL(package)
+for package in selectedElements:
+    package2OCL(package)
 
 
 
 
 #---------------------------------------------------------
-#           User interface for the Transformation 
+#           User interface for the Transformation
 #---------------------------------------------------------
 # The code below makes the link between the parameter(s)
-# provided by the user or the environment and the 
+# provided by the user or the environment and the
 # transformation functions above.
 # It also produces the end result of the transformation.
 # For instance the output can be written in a file or
